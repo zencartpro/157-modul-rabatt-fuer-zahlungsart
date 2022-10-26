ce29196e78a87d2a91277f62aa301f9565cb731e
Jörg Schönberger <office@webchills.at>
2022-10-26 17:10:58 +0200

    prevent blank page if module not yet installed

--- NEUE DATEIEN/includes/templates/DEINTEMPLATE/auto_loaders/loader_ot_payment_method_discount.php
+++ NEUE DATEIEN/includes/templates/DEINTEMPLATE/auto_loaders/loader_ot_payment_method_discount.php
@@ -6,9 +6,10 @@
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: loader_ot_payment_method_discount 2022-10-04 14:59:16Z webchills $
- */                               
-if (MODULE_ORDER_TOTAL_PAYMENT_METHOD_DISCOUNT_ENABLED == 'true') {                                                            
+ * @version $Id: loader_ot_payment_method_discount 2022-10-26 17:06:16Z webchills $
+ */  
+                            
+if (defined('MODULE_ORDER_TOTAL_PAYMENT_METHOD_DISCOUNT_ENABLED') && (MODULE_ORDER_TOTAL_PAYMENT_METHOD_DISCOUNT_ENABLED == 'true')) {                                                            
   $loaders[] = array('conditions' => array('pages' => array('checkout', 'quick_checkout')),
 										  'jscript_files' => array(
 										  'jquery/jquery_ot_payment_method_discount.js' => 1										
